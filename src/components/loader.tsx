355c3de8f0c4b2509d8167fc62cb515b1273f173
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2020-12-14 05:05:45 +0200

    add loader

---
+++ src/components/loader.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Loader() {
+  return <article className="container flex-column justify-content-center">
+    <h2>Loading...</h2>
+  </article>;
+}
+
+export default Loader;
