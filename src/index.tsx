dac83a4fa37669006fd181ce66f3166f1a8706f9
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2020-12-14 05:03:41 +0200

    replace App, change router, add connect

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from "./pages/App";
+import {Provider} from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
-  <App/>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/pages/App.tsx
@@ -0,0 +1,63 @@
+import React, {useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../assets/styles/App.scss';
+
+import {Route, Router, Switch} from 'react-router-dom';
+import {connect} from "react-redux";
+import Header from "../components/header";
+import {history} from "../redux/store";
+import DashboardContainer from "./dashboard/dashboardContainer";
+import BetaAccessContainer from "./betaAccess/betaAccessContainer";
+import Footer from "../components/footer";
+import {checkAuthToken} from "../redux/actions/authActionCreators";
+import NotFound from "./notFound";
+
+interface AppProps {
+  loading: boolean,
+  isAuthorized: boolean,
+  error: {},
+  checkAuthToken: any
+}
+
+function App(props: AppProps) {
+  useEffect(() => {
+    if (!props.isAuthorized) {
+      props.checkAuthToken();
+    }
+  }, [props.isAuthorized]);
+
+  return (
+    <main className="app main-content">
+      <div className="-padding-top80"/>
+      <Router history={history}>
+        <>
+          <Header auth={props.isAuthorized}/>
+
+          <Switch>
+            <Route exact={true} path="/" component={DashboardContainer}/>
+            <Route exact={true} path="/access-denied"
+                   component={BetaAccessContainer}/>
+            <Route path="*" component={NotFound}/>
+          </Switch>
+
+          <Footer/>
+        </>
+      </Router>
+    </main>
+  );
+}
+
+const mapStateToProps = (state: any) => ({
+  loading: state.auth.loading,
+  isAuthorized: state.auth.isAuthorized,
+  error: state.auth.error
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+  checkAuthToken: () => dispatch(checkAuthToken())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
