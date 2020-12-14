184272b187dd7a25ba0f8a1ea31bd705d826584f
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2020-12-14 04:58:00 +0200

    implements actioncreators, reducer, update constants

--- src/redux/actions/authActionCreators.ts
+++ src/redux/actions/authActionCreators.ts
@@ -1,5 +1,106 @@
-export function checkAuthCode() {
+import axios, {AxiosResponse} from "axios";
+import {
+  CHECK_AUTH_CODE_FAILURE, CHECK_AUTH_CODE_REQUEST,
+  CHECK_AUTH_CODE_SUCCESS,
+  FETCH_AUTH_CODE_FAILURE,
+  FETCH_AUTH_CODE_REQUEST,
+  FETCH_AUTH_CODE_SUCCESS
+} from "../constants/authConstants";
+import {getAuthorizedUserToken, setAuthorizedUserToken} from "../../utils/cookiesHandlers";
+import {Simulate} from "react-dom/test-utils";
+import {CODE_NOT_EXISTS} from "../../utils/errorTexts";
+
+
+export function checkAuthToken() {
+  return async (dispatch: DispatchAuth) => {
+    dispatch(authRequest());
+    let token = getAuthorizedUserToken();
+
+    if (token !== null && token) {
+      dispatch(authSuccess(true));
+    } else {
+      // let error = {message: 'Token not found'};
+      dispatch(authFailed(false));
+    }
+  }
 }
 
-export function fetchAuthCode() {
+export function compareAuthCodes(codes: Array<string>, currentCode: string) {
+  return async (dispatch: DispatchAuth) => {
+    dispatch(authRequest());
+
+    let isAuthCodeExists: number = codes.indexOf(currentCode);
+
+    if (isAuthCodeExists >= 0) {
+      setAuthorizedUserToken(currentCode);
+      dispatch(authSuccess(true));
+    } else {
+      let error = {message: CODE_NOT_EXISTS};
+      dispatch(authFailed(false, error));
+    }
+  }
+}
+
+export function fetchAuthCodes() {
+  return async (dispatch: DispatchAuth) => {
+    dispatch(fetchAuthCodesRequest());
+
+    await axios.get(
+      `https://api.mocki.io/v1/${process.env.REACT_APP_MOCK_API_KEY}`
+    ).then((authCodes: AxiosResponse) => {
+      if (authCodes != null) {
+        dispatch(fetchAuthCodesSuccess(authCodes));
+      }
+    }).catch((error) => {
+      dispatch(fetchAuthCodesFailure(error));
+    });
+
+  }
+}
+
+function fetchAuthCodesRequest(): AuthAction {
+  const action: AuthAction = {
+    type: FETCH_AUTH_CODE_REQUEST
+  };
+  return action;
+}
+
+function fetchAuthCodesSuccess(authCodes: any): AuthAction {
+  const action: AuthAction = {
+    type: FETCH_AUTH_CODE_SUCCESS,
+    payload: authCodes.data.codes
+  };
+  return action;
+}
+
+function fetchAuthCodesFailure(error: any): AuthAction {
+  const action: AuthAction = {
+    type: FETCH_AUTH_CODE_FAILURE,
+    payload: error
+  };
+  return action;
+}
+
+function authRequest() {
+  const action: AuthAction = {
+    type: CHECK_AUTH_CODE_REQUEST,
+  };
+  return action;
+}
+
+function authSuccess(isAuthorized: boolean) {
+  const action: AuthAction = {
+    type: CHECK_AUTH_CODE_SUCCESS,
+    payload: isAuthorized
+  };
+  return action;
+}
+
+function authFailed(isAuthorized: boolean, error?: {} | string) {
+  const action: AuthAction = {
+    type: CHECK_AUTH_CODE_FAILURE,
+    payload: isAuthorized,
+    additionalProp: error,
+  };
+  return action;
 }
--- src/redux/reducers/authReducer.ts
+++ src/redux/reducers/authReducer.ts
@@ -1,8 +1,58 @@
+import {
+  CHECK_AUTH_CODE_FAILURE, CHECK_AUTH_CODE_REQUEST, CHECK_AUTH_CODE_SUCCESS,
+  FETCH_AUTH_CODE_FAILURE,
+  FETCH_AUTH_CODE_REQUEST,
+  FETCH_AUTH_CODE_SUCCESS
+} from "../constants/authConstants";
+
 const initialState: AuthState = {
   loading: true,
-  recipesList: [],
+  codesList: [],
+  isAuthorized: false,
   error: null
 };
 
 export const authReducer = (state: AuthState = initialState, action: AuthAction) => {
+  switch (action.type) {
+    case FETCH_AUTH_CODE_REQUEST:
+      return {
+        ...state,
+        loading: true
+      };
+    case FETCH_AUTH_CODE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        codesList: action.payload,
+        error: null
+      };
+    case FETCH_AUTH_CODE_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        codesList: [],
+        error: action.payload
+      };
+    case CHECK_AUTH_CODE_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case CHECK_AUTH_CODE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isAuthorized: action.payload,
+        error: null
+      };
+    case CHECK_AUTH_CODE_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        isAuthorized: action.payload,
+        error:  action.additionalProp
+      };
+    default:
+      return state
+  }
 };
