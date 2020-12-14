4a940d990710384885f3f4c6ece6d900202ec92e
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2020-12-14 05:07:21 +0200

    create dashboard container

---
+++ src/pages/dashboard/dashboardContainer.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Dashboard from "./dashboard";
+import {connect} from "react-redux";
+import Loader from "../../components/loader";
+import {history} from "../../redux/store";
+
+interface DashboardProps {
+  loading: boolean,
+  isAuthorized: boolean
+}
+
+function DashboardContainer(props: DashboardProps) {
+  if (props.loading) {
+    return <Loader/>
+  }
+
+  if (!props.isAuthorized) {
+    history.replace('/access-denied');
+  }
+
+  return<Dashboard/>;
+}
+
+const mapStateToProps = (state: any) => ({
+  loading: state.auth.loading,
+  isAuthorized: state.auth.isAuthorized
+});
+
+const mapDispatchToProps = (dispatch: any) => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DashboardContainer);
