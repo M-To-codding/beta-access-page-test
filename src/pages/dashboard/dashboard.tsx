ac9b6013b4bfc7b8e7cec1e596ecd03e083d66e9
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2020-12-14 05:06:18 +0200

    add styles to layout

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Dashboard() {
 
-  return <article className="container">
-    <h1>Welcome to dashboard!</h1>
-  </article>
+  return <article className="container d-flex justify-content-center">
+    <h1 className="title">Welcome to dashboard!</h1>
+  </article>;
 }
 
 export default Dashboard;
