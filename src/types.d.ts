683786cd8aa86042a493f0e47cee637317fc2212
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2020-12-14 05:04:06 +0200

    add types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,11 +1,44 @@
 interface AuthState {
-
+  loading: boolean,
+  codesList: Array<string>,
+  isAuthorized: boolean,
+  error: string | null
 }
 
 interface AuthAction {
-
+  type: string,
+  payload?: Array<string> | boolean,
+  additionalProp?: {} | string
 }
 
-interface IAccessCodes {
-  codes: Array<string>;
+type DispatchAuth = (args: AuthAction) => AuthAction;
+
+interface IPhoto {
+  large: string,
+  medium: string,
+  thumbnail: string
 }
+
+interface IUser {
+  id: string,
+  name: string,
+  surname: string,
+  gender: string,
+  dateOfBirth: string,
+  photo?: IPhoto,
+  phone: string,
+  email: string
+}
+
+interface UsersState {
+  loading: boolean,
+  usersList: Array<IUser>,
+  error: any
+}
+
+interface UsersAction {
+  type: string,
+  payload?: Array<any>
+}
+
+type DispatchUsers = (args: UsersAction) => UsersAction;
