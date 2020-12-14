f8404dbf1544f5297200c94ff780228752c6e8d0
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2020-12-14 05:06:36 +0200

    add notFound page

---
+++ src/pages/notFound.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function NotFound() {
+
+  return <article className="container">
+    <h3 className="title">Not found</h3>
+  </article>;
+}
+
+export default NotFound;
