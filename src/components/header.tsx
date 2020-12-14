e896c34c0ec84766f486d92b82a931f3ed77cf8b
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2020-12-14 05:22:15 +0200

    add header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {deleteCookie} from "../utils/cookiesHandlers";
 
 
-function Header() {
+interface HeaderProps {
+  auth: boolean
+}
+
+function Header(props: HeaderProps) {
   return <header className="header">
     <Link to="/">
       <h3>
         Header
       </h3>
     </Link>
+
+    {props.auth ?
+      <section>
+        <Link to="/users">Users </Link>
+        <a href="/" onClick={() => deleteCookie('auth_token')}>Logout</a>
+      </section>
+      : <></>}
   </header>;
 }
 
