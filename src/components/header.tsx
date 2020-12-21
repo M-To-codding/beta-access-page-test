1197e7e92824957608e48ddc429131b05233d62d
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2020-12-22 00:35:00 +0200

    pass logout reducer into header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import {deleteCookie} from "../utils/cookiesHandlers";
 
 
 interface HeaderProps {
-  auth: boolean
+  auth: boolean,
+  logOut: any
 }
 
 function Header(props: HeaderProps) {
@@ -18,7 +18,7 @@ function Header(props: HeaderProps) {
     {props.auth ?
       <section>
         <Link to="/users">Users </Link>
-        <a href="/" onClick={() => deleteCookie('auth_token')}>Logout</a>
+        <a href="/" onClick={() => props.logOut()}>Logout</a>
       </section>
       : <></>
     }
