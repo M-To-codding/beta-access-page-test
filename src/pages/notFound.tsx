93e6e6189df1b823af2b21beef17cc46539fdd49
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2020-12-23 18:40:36 +0200

    change layout and styles

--- src/pages/notFound.tsx
+++ src/pages/notFound.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function NotFound() {
 
-  return <article className="container">
-    <h3 className="title">Not found</h3>
+  return <article className="not-found -padding-top80">
+    <div className="container d-flex flex-column justify-content-between">
+      <h3 className="title">Not found</h3>
+      <button type="button" className="btn btn-warning col-xl-3 col-sm-4 main-btn align-self-center">Home</button>
+    </div>
   </article>
 }
 
