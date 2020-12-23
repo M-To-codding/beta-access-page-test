cfc847d360a54e4882ccda98784b121ef8f42c4a
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2020-12-23 18:41:16 +0200

    fix bootstrap import

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,7 +4,6 @@ import '../assets/styles/App.scss';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js.map';
 
 import {Route, Router, Switch} from 'react-router-dom';
 import {connect} from "react-redux";
