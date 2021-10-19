daaf0ec7e8f0ef5343f4a54341bed853e7a9dd97
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2021-10-19 19:52:31 +0300

    display products data from db

--- src/components/cards/productCard.tsx
+++ src/components/cards/productCard.tsx
@@ -2,23 +2,50 @@ import React from "react";
 
 
 interface ProductCardProps {
-  products: IProduct[]
+  product?: IProduct
 }
 
 function ProductCard(props: ProductCardProps) {
-  return <section className="col-xl-4 col-md-4">
-    <div className="card bg-light">
-      <img className="card-img-top"
-           src="https://res.cloudinary.com/patch-gardens/image/upload/c_fill,f_auto,h_840,q_auto:good,w_840/v1563806745/products/snake-plant-cc1865.jpg"
-           alt="Card image cap"/>
-      <div className="card-body">
-        <h5 className="card-title">Primary card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-          card's
-          content.</p>
+
+  if (!props.product) {
+    return <h3>Product not found</h3>;
+  }
+
+  let productGallery = props.product.gallery;
+
+  return (
+    <section className="col-xl-4 col-md-4">
+      <div className="card bg-light">
+
+        {productGallery.length <= 1 &&
+        <img className="card-img-top"
+             src={
+               productGallery.length === 0 ?
+                 'https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/512x512/plain/plant.png' :
+                 productGallery[0]
+             }
+             alt="Card image cap"/>
+        }
+
+        {productGallery.length > 1 &&
+        productGallery.map((imgItem) => {
+          return <img className="card-img-top"
+                      src={imgItem}
+                      alt="Card image cap"/>
+        })
+        }
+
+        <div className="card-body">
+          <h5 className="card-title">
+            {props.product?.title}
+          </h5>
+          <p className="card-text">
+            {props.product?.description}
+          </p>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  )
 }
 
 export default ProductCard;
