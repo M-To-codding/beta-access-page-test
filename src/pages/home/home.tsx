31b018988a37977027b5a3288e23c395d551c749
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2021-10-19 19:55:29 +0300

    map array of products and pass data to product card component

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,13 +3,38 @@ import Loader from "../../components/loader";
 import Carousel from "../../components/carousel";
 import ProductCard from "../../components/cards/productCard";
 
-function Home() {
+
+interface AppProps {
+  // loading: boolean,
+  //   // isAuthorized: boolean,
+  //   // error: {},
+  //   // checkAuthToken: any,
+  //   // logOut: any
+  productsList: IProduct[],
+}
+
+function Home(props: AppProps) {
+  if (!props.productsList || props.productsList.length === 0) {
+    return <article className="home-content">
+      <Carousel/>
+
+      <section className="container -padding-top80 -padding-bottom80">
+        <h3 className="text-center">Products list is empty</h3>
+      </section>
+    </article>
+  }
+
   return <article className="home-content">
     <Carousel/>
 
     <section className="container -padding-top80 -padding-bottom80">
       <div className="row">
-        <ProductCard products={[]}/>
+        {
+          (props.productsList && props.productsList.length > 0) &&
+          props.productsList.map((item: IProduct) => {
+            return <ProductCard product={item}/>;
+          })
+        }
       </div>
     </section>
   </article>
