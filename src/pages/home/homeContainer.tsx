3c5fda17d0124e1ecfb4461d762d6d8fbb6ae914
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2021-10-19 19:55:13 +0300

    get products, pass data to home page

--- src/pages/home/homeContainer.tsx
+++ src/pages/home/homeContainer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Dashboard from "../dashboard/dashboard";
 import {connect} from "react-redux";
 import Loader from "../../components/loader";
@@ -6,14 +6,23 @@ import {history} from "../../redux/store";
 import {authorizationFailure} from "../../redux/actions/authActionCreators";
 import {USER_NOT_AUTHORIZED} from "../../utils/errorTexts";
 import Home from "./home";
+import {getProductsActionCreators} from "../../redux/actions/products/getProductsActionCreators";
 
 interface HomeProps {
   loading: boolean,
   isAuthorized: boolean,
-  authorizationFailure: any
+  authorizationFailure: any,
+  getProducts: any,
+  productsList: any
 }
 
 function HomeContainer(props: HomeProps) {
+  let productsList = props.productsList;
+
+  useEffect(() => {
+    props.getProducts();
+  }, []);
+
   if (props.loading) {
     return <Loader/>
   }
@@ -22,17 +31,19 @@ function HomeContainer(props: HomeProps) {
     return <Dashboard/>
   }
 
-  return <Home/>
+  return <Home productsList={productsList}/>
 }
 
 const mapStateToProps = (state: any) => ({
   loading: state.auth.loading,
-  isAuthorized: state.auth.isAuthorized
+  isAuthorized: state.auth.isAuthorized,
+  productsList: state.products.productsList
 });
+
 
 const mapDispatchToProps = (dispatch: any) => ({
   authorizationFailure: (data: any) => dispatch(authorizationFailure(data)),
-  getProducts: (data: any) => dispatch(authorizationFailure(data))
+  getProducts: (data: any) => dispatch(getProductsActionCreators(data))
 });
 
 export default connect(
