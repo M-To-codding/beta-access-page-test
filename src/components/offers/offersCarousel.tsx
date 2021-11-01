ce7ddff5410302c3cdc6bdef329aac7ec18099cf
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2021-11-02 01:32:04 +0200

    remove unnecessary inline styles, change layout, pass new props to carousel

--- src/components/offers/offersCarousel.tsx
+++ src/components/offers/offersCarousel.tsx
@@ -4,11 +4,12 @@ import ProductCarousel from "../productCarousel";
 
 function OffersCarousel() {
 
-  const slickData = [{
-    id: '0',
-    url: 'https://sgi2.offerscdn.net/i/production/published/65/filename/14983.flim.v63.bffffff.jpg',
-    caption: 'Stuff offers'
-  },
+  const slickData = [
+    {
+      id: '0',
+      url: 'https://sgi2.offerscdn.net/i/production/published/65/filename/14983.flim.v63.bffffff.jpg',
+      caption: 'Stuff offers'
+    },
     {
       id: '1',
       url: 'https://i.pinimg.com/originals/df/43/74/df43748883cf66a686c48ede3508be10.jpg',
@@ -16,21 +17,14 @@ function OffersCarousel() {
     }
   ]
 
-  return <article className="container flex-column justify-content-center offers-content">
-    <section className="flex-row offers-list">
-
-
-      {/*<ProductCarousel carouselData={slickData} carouselClassName="offers-carousel"/>*/}
-
-      <div className="">
-
-      </div>
-
+  return <article className="flex-column justify-content-center offers-content -padding-bottom80">
+    <section className="d-flex flex-row offers-list">
+      <ProductCarousel carouselData={slickData} carouselClassName="offers-carousel-content" slidesToShow={2} autoplay={false} caption={true}/>
     </section>
 
     <a href="#">
       <section className="flex-row justify-content-between link-to-offers">
-        <div className="img-small-circle" style={{height: '60px', objectFit: 'cover'}}>
+        <div className="img-small-circle">
           <img
             src="https://imgs.michaels.com/MAM/assets/1/726D45CA1C364650A39CD1B336F03305/img/30402E408CEC494B8197AFC3E8ECAFAB/28529_32606328.jpg?fit=inside|1024:1024"
             alt=""/>
@@ -40,7 +34,7 @@ function OffersCarousel() {
           Lorem ipsum dolor sit amet
         </p>
 
-        <div className="img-small-circle" style={{height: '60px', objectFit: 'cover'}}>
+        <div className="img-small-circle">
           <img width="auto"
                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLf9FbqfyHSq-TQNZoiP3BwpBsMvgl-cWFKpFRYOO2opd_-nTt23aZfNWjbGwh2zrlvmI&usqp=CAU"
                alt=""/>
