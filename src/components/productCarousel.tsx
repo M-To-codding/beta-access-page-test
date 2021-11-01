c5f2ad0a5e555270cadc436acb04acffe65a0bf8
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2021-11-02 01:21:26 +0200

    pass new props to carousel, add caption

--- src/components/productCarousel.tsx
+++ src/components/productCarousel.tsx
@@ -1,33 +1,45 @@
 import React, {useEffect} from "react";
 import Slider from "react-slick";
 
-function ProductCarousel(props:any) {
-  const carouselData: Array<SlickImage> = props.carouselData;
+interface ProductCarouselProps {
+  carouselData: Array<SlickImage>,
+  carouselClassName?: string,
+  slidesToShow?: number,
+  autoplay?: boolean,
+  caption?: boolean
+}
+
+function ProductCarousel(props: ProductCarouselProps) {
+  const {carouselData, carouselClassName = '', slidesToShow = 1, autoplay = true, caption = false}: ProductCarouselProps = props;
 
   const settings = {
     cssEase: 'linear',
     dots: true,
     infinite: true,
-    autoplay: true,
+    autoplay: autoplay,
     speed: 1000,
     duration: 1000,
-    slidesToShow: 1,
+    slidesToShow: slidesToShow,
     slidesToScroll: 1,
     arrows: false
   };
 
-  useEffect(() => {
-  });
 
-  return <section className="slick-product-carousel-content">
+  return <section className={`slick-product-carousel-content ${carouselClassName}`}>
     <Slider {...settings} className="slick-product-carousel">
-        {
-          carouselData.map((item: SlickImage) => {
-            return <div key={'img' + item.id} className="slider-item" style={{"height": "100px"}}>
-              <img className="d-block w-100" src={item.url} alt="First slide"/>
-            </div>;
-          })
-        }
+      {
+        carouselData.map((item: SlickImage) => {
+          return <div key={'img' + item.id} className="slider-item" style={{"height": "100px"}}>
+            {
+              caption &&
+              <a href="#" className="caption-link">
+                <h4 className="caption">{item.caption}</h4>
+              </a>
+            }
+            <img className="d-block w-100" src={item.url} alt="First slide"/>
+          </div>;
+        })
+      }
     </Slider>
   </section>
 }
