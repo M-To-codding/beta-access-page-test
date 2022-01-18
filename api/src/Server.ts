00c979270e9c523f839174da163b0d22bba7cb39
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2022-01-18 11:36:01 +0200

    config: add cors settings

--- api/src/Server.ts
+++ api/src/Server.ts
@@ -25,6 +25,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser(cookieProps.secret));
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    next();
+});
+
 // Show routes called in console during development
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
