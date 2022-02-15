4ee52e372763f48ea56f61c5036ab95365251c03
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2022-02-15 17:33:50 +0200

    style: add sections to footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -3,7 +3,14 @@ import React from "react";
 
 function Footer() {
   return <footer className="footer">
-    <p className="copyright -small-text14 -opacity50">Copyright 2020</p>
+
+    <section>
+      <h1 className="">Logo</h1>
+    </section>
+
+    <section>
+      <p className="copyright -small-text14 -opacity50">Copyright 2020</p>
+    </section>
   </footer>
 }
 
