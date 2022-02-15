d9f686b236c901a2b8fd522a8ccfe26d4ba98ce0
Todoryshyna-Maryna <marina.todorishina@gmail.com>
2022-02-15 22:42:34 +0200

    refactor: replace instagram photos list into separated component

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -7,6 +7,7 @@ import StoreButtonBanner from "../../components/buttons/storeButtonBanner";
 import SpecialGiftOffer from "../../components/specialGiftOffer";
 import VideoBanner from "../../components/videoBanner";
 import RecentNewsList from "../../components/recentNewsList";
+import InstagramPhotosList from "../../components/instagramPhotosList";
 
 
 interface AppProps {
@@ -52,39 +53,7 @@ function Home(props: AppProps) {
     <OffersListContainer/>
     <VideoBanner/>
     <RecentNewsList/>
-    {/* Instagram block*/}
-
-    <section>
-      <a href="#" className="d-flex">
-        <article>
-          <img width={300}
-               src="https://specials-images.forbesimg.com/imageserve/608990c4bcf2c7b4802c9725/Indoor-plants/960x0.jpg?fit=scale"
-               alt=""/>
-        </article>
-        <article>
-          <img width={300}
-               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV-V60P4f_c5jNgCpiIRubR2kjveE0rgmJ6A&usqp=CAU"
-               alt=""/>
-        </article>
-        <article>
-          <img width={300}
-               src="https://www.ugaoo.com/knowledge-center/wp-content/uploads/2016/07/moss-plants-blog-banner-850x525.jpg"
-               alt=""/>
-        </article>
-        <article>
-          <img width={300}
-               src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1589561181-Monet_SolidCarousel.jpg?crop=0.870xw:0.906xh;0.0353xw,0.0762xh"
-               alt=""/>
-        </article>
-        <article>
-          <img width={300}
-               src="https://specials-images.forbesimg.com/imageserve/608990c4bcf2c7b4802c9725/Indoor-plants/960x0.jpg?fit=scale"
-               alt=""/>
-        </article>
-      </a>
-    </section>
-
-    {/* /Instagram block*/}
+    <InstagramPhotosList/>
 
     {/* Subscription block*/}
 
